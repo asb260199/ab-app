bfa7a0a5528073bc9ea9b2d366ef6a0df27b3029
Akshay <asb260199@gmail.com>
2023-05-01 13:51:29 +0530

    e

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 export default function Nav() {
   const [showMenu, setShowMenu] = useState(false);
+  const [home, setHome] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
@@ -14,12 +15,16 @@ export default function Nav() {
     setShowMenu(false);
   };
 
+  const handleHome = () => {
+    setHome(true);
+  };
+
   return (
     <>
     <nav className="nav">
       <div className="nav-logo">
         <Link to="/">
-          <img src="logo.png" alt="Logo" />
+          <img src="logo.png" alt="Logo" onClick={function(event){handleHome()}} />
         </Link>
       </div>
       <div className="nav-hamburger" onClick={toggleMenu}>
@@ -28,31 +33,40 @@ export default function Nav() {
         <div className="hamburger-line"></div>
       </div>
       <div className={`nav-links ${showMenu ? 'show-menu' : ''}`}>
-      <div className='cross'><img src="close.png" alt="cross" onClick={handleClick} /></div>
+      <div className='cross'><img src="close.png" alt="cross" onClick={function(event){handleClick();handleHome()}} /></div>
         <ul>
           <li>
-            <Link to="/" onClick={handleClick}>
+            <Link to="/" onClick={function(event){handleClick();handleHome()}}>
               Home
             </Link>
           </li>
           <li>
-            <Link to="/Abt" onClick={handleClick}>
+            <Link to="/Abt" onClick={function(event){handleClick();handleHome()}}>
               About
             </Link>
           </li>
           <li>
-            <Link to="/Contact" onClick={handleClick}>
+            <Link to="/Contact" onClick={function(event){handleClick();handleHome()}}>
               Contact Me
             </Link>
           </li>
           <li>
-            <Link to="/Admin" onClick={handleClick}>
+            <Link to="/Admin" onClick={function(event){handleClick();handleHome()}}>
               Admin
             </Link>
           </li>
         </ul>
       </div>
     </nav>
+    <div className={`header-content ${home ? 'home-hide' : ''}`}>
+      <div className="h1"> Hi, I am</div>
+      <div className="h2">Akshay Sudhakar Bodke</div>
+      <div className="h3">Software Development Engineer & a Trader</div>
+      <div className="btns">
+          <Link to="https://asb260199.github.io/ab-app/abr.pdf" target="_blank"><button className="btn">Download CV</button></Link>
+          <Link to="/Contact"><button className="btn" onClick={function(event){handleHome()}}>Contact Me</button></Link>
+      </div>
+    </div>
     </>
   );
 };
